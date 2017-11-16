d09d482670b190cdbb7fc26cb3969dd6be9b106d
latikapatil21 <33638817+latikapatil21@users.noreply.github.com>
2017-11-16 13:13:33 -0500

    Update MessageService.java

--- src/main/java/net/petrikainulainen/gradle/core/MessageService.java
+++ src/main/java/net/petrikainulainen/gradle/core/MessageService.java
@@ -6,6 +6,6 @@ package net.petrikainulainen.gradle.core;
 public class MessageService {
 
     public String getMessage() {
-        return "Hello World!";
+        return "Hello new World!";
     }
 }
